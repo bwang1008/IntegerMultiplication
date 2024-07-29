2c6f63449555d796ff476b15c368c1286aa3c286
bwang1008 <bwang1008@gmail.com>
2024-07-29 00:57:26 -0500

    Add more documentation to Tape class and remove unused function

--- integer_multiplication/turing_machine/tape.py
+++ integer_multiplication/turing_machine/tape.py
@@ -15,24 +15,27 @@ class Tape:
     """Class defining tape of a Turing machine."""
 
     def __init__(self) -> None:
-        """Initialize head pointer and tape cells."""
+        """Initialize head pointer and tape cells to default BLANK."""
         self.head: int = 0
         self.cells: defaultdict = defaultdict(lambda: Symbol.BLANK)
 
     def read(self) -> Symbol:
-        """Get symbol at head of tape."""
+        """Get symbol at head of tape.
+
+        :return: Symbol at the tape head
+        """
         return self.cells[self.head]
 
     def write(self, symbol: Symbol) -> None:
-        """Set symbol at head of tape."""
+        """Set symbol at head of tape.
+
+        :param symbol: Symbol to be written at the head of the tape
+        """
         self.cells[self.head] = symbol
 
     def shift(self, shift_type: Shift) -> None:
-        """Shift the head by the amount specified."""
-        self.head += shift_type.value
+        """Shift the head by the amount specified.
 
-    def set_input(self, starting_symbols: list[Symbol]) -> None:
-        """Set the initial tape contents, moving head right."""
-        for symbol in starting_symbols:
-            self.cells[self.head] = symbol
-            self.head += 1
+        :param shift_type: which way the tape head should shift after a transition
+        """
+        self.head += shift_type.value

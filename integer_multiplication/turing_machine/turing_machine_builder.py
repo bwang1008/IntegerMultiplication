1ffade4ea3e242e71511f17d47b82c78db2f250e
bwang1008 <bwang1008@gmail.com>
2024-07-29 00:51:15 -0500

    Add more documentation to Turing machine helper class

--- integer_multiplication/turing_machine/turing_machine_builder.py
+++ integer_multiplication/turing_machine/turing_machine_builder.py
@@ -60,7 +60,13 @@ class TuringMachineBuilder:
         return self.create_state(halting=halting)
 
     def get_or_create_tape_index(self, *, name: str) -> int:
-        """Fetch an existing tape index or create a new tape."""
+        """Fetch an existing tape index or create a new tape.
+
+        :param name: name associated with the tape. If used before, the existing
+            tape index is returned. Otherwise a new tape is created with the
+            associated name.
+        :return: Index of existing / new tape
+        """
         if name in self.named_tapes:
             return self.named_tapes[name]
 
@@ -79,7 +85,15 @@ class TuringMachineBuilder:
         self.starting_state = starting_state
 
     def add_transition_general(self, old_state: int, transition: Transition) -> None:
-        """Add a Transition instance associated with old_state."""
+        """Add a Transition instance associated with old_state.
+
+        :param old_state: which state the Turing machine is in to consider
+            this transition
+        :param transition: Transition instance detailing which new state to
+            move to, which tape indices need to be read to check acceptance,
+            which new symbols to write on the tapes, and which way to shift
+            the tape heads
+        """
         self.transitions[old_state].append(transition)
 
     def add_single_tape_transition(
@@ -89,7 +103,23 @@ class TuringMachineBuilder:
         tape_index: int,
         single_transition: SingleTapeTransition,
     ) -> None:
-        """Create a transition between old state to new state based on one tape."""
+        """Create a transition between old state to new state based on one tape.
+
+        Often not all of the tapes are used when considering to take a transition
+        at the current state. When only one tape is considered, this effectively
+        reduces to a transition from a 1-tape Turing machine. Then the machinery
+        for specifying which tape indices to read/write/shift do not need to be
+        specified so many times, leading to a more simplified interface.
+
+        :param old_state: which state the Turing machine is in to consider
+            this transition
+        :param new_state: the state the Turing machine should be in when taking
+            this transition
+        :param tape_index: which tape index to read/write/shift from
+        :param single_transition: container object with what character to
+            expect to accept this transition, what new symbol to write, and
+            how to shif tape_index's tape head
+        """
         self.add_transition_general(
             old_state,
             Transition(
@@ -105,6 +135,9 @@ class TuringMachineBuilder:
 
         A starting state should be set first with set_starting_state() before
         calling this method.
+
+        :return: TuringMachine instance corresponding to states/tapes/transitions
+            previously added
         """
         if self.starting_state is None:
             error_msg: str = "Must set starting_state before creating Turing machine."

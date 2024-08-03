f00380b6154c854b307f7fe31f19f1b9b7bb7e1a
bwang1008 <bwang1008@gmail.com>
2024-08-02 22:15:59 -0500

    Move half-adder logic to function

--- integer_multiplication/algorithm/grade_school.py
+++ integer_multiplication/algorithm/grade_school.py
@@ -1,11 +1,71 @@
 """Implementation of grade-school integer multiplication on Turing machine."""
 
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
 from integer_multiplication.turing_machine.shift import Shift
 from integer_multiplication.turing_machine.symbol import Symbol
-from integer_multiplication.turing_machine.turing_machine import TuringMachine
 from integer_multiplication.turing_machine.turing_machine_builder import (
     TuringMachineBuilder,
 )
+
+if TYPE_CHECKING:
+    from integer_multiplication.turing_machine.turing_machine import TuringMachine
+
+
+@dataclass
+class TapeDirection:
+    """Class for keeping track of a specific tape and a direction/shift."""
+
+    tape_index: int
+    shift: Shift
+
+
+def _create_half_adder_transitions(
+    output_tape: TapeDirection,
+    summand_tape: TapeDirection,
+    carry_tape: int,
+) -> list[tuple[dict[int, str], dict[int, Symbol], dict[int, Shift]]]:
+    """Create transitions corresponding to half-adder inputs and outputs.
+
+    :param output_tape: which tape the output is stored on, as well as which
+        direction / shift to go from least to most significant bit.
+    :param summand_tape: which tape that should be added to output.
+    :param carry_tape: tape that holds the carry bit, initially assigned to 0
+    :return: the state that the transitions end on
+    """
+    transitions: list[tuple[dict[int, str], dict[int, Symbol], dict[int, Shift]]] = []
+    for output_value in [[Symbol.BLANK.value, Symbol.ZERO.value], Symbol.ONE.value]:
+        for arg1_value in [Symbol.ZERO.value, Symbol.ONE.value]:
+            for carry_value in [Symbol.ZERO.value, Symbol.ONE.value]:
+                # add the 3 bits in the 3 tapes
+                triple_sum: int = [output_value, arg1_value, carry_value].count(
+                    Symbol.ONE.value
+                )
+
+                # split into 2 bits: the one you write, and the one to be carried over
+                write_bit: int = triple_sum & 1
+                carry_bit: int = (triple_sum // 2) & 1
+
+                accept_condition = {
+                    output_tape.tape_index: output_value,
+                    summand_tape.tape_index: arg1_value,
+                    carry_tape: carry_value,
+                }
+                symbols_to_write = {
+                    output_tape.tape_index: Symbol[str(write_bit)],
+                    carry_tape.tape_index: Symbol[str(carry_bit)],
+                }
+                tape_shifts = {
+                    output_tape.tape_index: output_tape.shift,
+                    summand_tape.tape_index: summand_tape.shift,
+                }
+
+                transitions.append((accept_condition, symbols_to_write, tape_shifts))
+
+    return transitions
 
 
 def create_grade_school_turing_machine() -> TuringMachine:
@@ -83,37 +143,30 @@ def create_grade_school_turing_machine() -> TuringMachine:
     # least signficant bit of the sum to output_tape, while the 2nd-least
     # significant bit is written to the carry_tape.
     # Note that output_tape starting as BLANK should be treated the same as
-    # starting as 0.
-    for output_value in [[Symbol.BLANK.value, Symbol.ZERO.value], Symbol.ONE.value]:
-        for arg1_value in [Symbol.ZERO.value, Symbol.ONE.value]:
-            for carry_value in [Symbol.ZERO.value, Symbol.ONE.value]:
-                # add the 3 bits in the 3 tapes
-                triple_sum: int = [output_value, arg1_value, carry_value].count(
-                    Symbol.ONE.value
-                )
+    # starting as 1.
 
-                # split into 2 bits: the one you write, and the one to be carried over
-                write_bit: int = triple_sum & 1
-                carry_bit: int = (triple_sum // 2) & 1
+    half_adder_transitions: list[
+        tuple[dict[int, str], dict[int, Symbol], dict[int, Shift]]
+    ] = _create_half_adder_transitions(
+        TapeDirection(output_tape, Shift.LEFT),
+        TapeDirection(arg1_tape, Shift.LEFT),
+        carry_tape,
+    )
 
-                builder.add_transition(
-                    process_arg2_node,
-                    new_state=process_arg2_node,
-                    accept_condition={
-                        input_tape: Symbol.ONE.value,
-                        output_tape: output_value,
-                        arg1_tape: arg1_value,
-                        carry_tape: carry_value,
-                    },
-                    symbols_to_write={
-                        output_tape: Symbol[str(write_bit)],
-                        carry_tape: Symbol[str(carry_bit)],
-                    },
-                    tape_shifts={
-                        output_tape: Shift.LEFT,
-                        arg1_tape: Shift.LEFT,
-                    },
-                )
+    for accept_condition, symbols_to_write, tape_shifts in half_adder_transitions:
+        builder.add_transition(
+            process_arg2_node,
+            new_state=process_arg2_node,
+            accept_condition={
+                input_tape: Symbol.ONE.value,
+                **accept_condition,
+            },
+            symbols_to_write=symbols_to_write,
+            tape_shifts=tape_shifts,
+        )
 
-    # if arg1 tape sees a blank, moved all the way to the left of arg 1.
-    # need to start
+    # if arg1 tape sees a blank, then the head has already moved all the way
+    # to the left of arg 1. Here, we need to move the head of the arg1 tape
+    # back to the right, the least-significant bit, just like performing the
+    # grade-school multiplication algorithm on paper where you write the next
+    # summand on the next line starting from the least significant bit.

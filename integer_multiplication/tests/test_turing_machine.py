2bd05ed068a17270d4b2211d5923cc14e91144ef
bwang1008 <bwang1008@gmail.com>
2024-08-11 03:05:23 -0500

    Add another test

--- integer_multiplication/tests/test_turing_machine.py
+++ integer_multiplication/tests/test_turing_machine.py
@@ -1,14 +1,17 @@
 """Tests behavior of Turing machine class."""
 
+import pytest
+
 from integer_multiplication.turing_machine.shift import Shift
 from integer_multiplication.turing_machine.symbol import Symbol
 from integer_multiplication.turing_machine.transition import Transition
 from integer_multiplication.turing_machine.turing_machine import TuringMachine
 
 
-def test_turing_machine_set_input_tape_values() -> None:
-    """Check that setting input is on tape 0 and has expected symbols."""
-    tm: TuringMachine = TuringMachine(
+@pytest.fixture()
+def simple_tm() -> TuringMachine:
+    """Pytest fixture for common Turing machine to test on."""
+    return TuringMachine(
         num_states=2,
         num_tapes=1,
         starting_state=0,
@@ -25,15 +28,28 @@ def test_turing_machine_set_input_tape_values() -> None:
         ],
     )
 
-    tm.set_input_tape_values(
+
+def test_set_input_tape_values(simple_tm: TuringMachine) -> None:
+    """Check that setting input is on tape 0 and has expected symbols."""
+    simple_tm.set_input_tape_values(
         [Symbol.ONE, Symbol.ZERO, Symbol.BLANK, Symbol.ONE], reset_tape_head=True
     )
 
-    assert len(tm.tapes) == 1
-    assert tm.tapes[0].head == 0
-    assert tm.tapes[0].cells == {
+    assert len(simple_tm.tapes) == 1
+    assert simple_tm.tapes[0].head == 0
+    assert simple_tm.tapes[0].cells == {
         0: Symbol.ONE,
         1: Symbol.ZERO,
         2: Symbol.BLANK,
         3: Symbol.ONE,
     }
+
+
+def test_set_input_tape_values_fails_after_run(simple_tm: TuringMachine) -> None:
+    """Check that setting input after a run begins errors."""
+    simple_tm.num_steps = 1
+
+    with pytest.raises(RuntimeError):
+        simple_tm.set_input_tape_values(
+            [Symbol.ONE, Symbol.ZERO, Symbol.BLANK, Symbol.ONE], reset_tape_head=True
+        )
